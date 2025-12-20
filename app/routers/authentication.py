0e20637afbee0b8bf47ce41d83b4454ef150065a
Roshika Chitrakar <164656945+RoshikaC@users.noreply.github.com>
2025-12-20 22:37:34 +0545

    Updated routers

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -1,3 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,HTTPException,status
+from sqlalchemy.orm import Session
+from .. import database,schemas,models,utils,crud
 
 router=APIRouter()
+
+@router.post("/signup",response_model=schemas.UserResponse)
+def create_user(user:schemas.UserCreate,db:Session=Depends(database.get_db)):
+    db_user=crud.get_user_by_email(db,email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400,detail="Email already registered")
+    return crud.create_user(db=db,user=user)
+
+@router.post("/login")
+def login(user_credentials:schemas.UserLogin,db:Session=Depends(database.get_db)):
+    user=crud.get_user_by_email(db,email=user_credentials.email)
+    if not user or not utils.Hash.verify(user_credentials.password,user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Invalid Credentials")
+    return {"message":"Login Successful","user_id":user.id}
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,3 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from typing import List
+from .. import database,models,schemas
 
 router=APIRouter()
+
+@router.get("/",response_model=List[schemas.UserResponse])
+def read_all_users(skip:int=0,limit:int=100,db:Session=Depends(database.get_db)):
+    users=db.query(models.User).offset(skip).limit(limit).all()
+    return users
+
+@router.get("/{user_id}",response_model=List[schemas.UserResponse])
+def read_user(user_id:int,db:Session=Depends(database.get_db)):
+    user=db.query(models.User).filter(models.User.id==user_id).first()
+    return user
