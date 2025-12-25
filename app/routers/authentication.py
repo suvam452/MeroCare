f0ac13bad39708e92cee0857773abaa0956ecee9
Roshika Chitrakar <164656945+RoshikaC@users.noreply.github.com>
2025-12-25 18:29:58 +0545

    Updated routes

--- app/routers/authentication.py
+++ app/routers/authentication.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import Session
-from .. import database,schemas,models,utils,crud
+from .. import database,schemas,models,utils,crud,oauth2
+from fastapi.security import OAuth2PasswordRequestForm
 
 router=APIRouter()
 
@@ -11,9 +12,10 @@ def create_user(user:schemas.UserCreate,db:Session=Depends(database.get_db)):
         raise HTTPException(status_code=400,detail="Email already registered")
     return crud.create_user(db=db,user=user)
 
-@router.post("/login")
+@router.post("/login",response_model=schemas.Token)
 def login(user_credentials:schemas.UserLogin,db:Session=Depends(database.get_db)):
     user=crud.get_user_by_email(db,email=user_credentials.email)
     if not user or not utils.Hash.verify(user_credentials.password,user.hashed_password):
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="Invalid Credentials")
-    return {"message":"Login Successful","user_id":user.id}
+    access_token=oauth2.create_access_token(data={"user_id":str(user.id)})
+    return {"access_token":access_token,"token_type":"bearer"}
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter,Depends
 from sqlalchemy.orm import Session
 from typing import List
-from .. import database,models,schemas
+from .. import database,models,schemas,oauth2
 
 router=APIRouter()
 
@@ -10,7 +10,11 @@ def read_all_users(skip:int=0,limit:int=100,db:Session=Depends(database.get_db))
     users=db.query(models.User).offset(skip).limit(limit).all()
     return users
 
-@router.get("/{user_id}",response_model=List[schemas.UserResponse])
+@router.get("/{user_id}",response_model=schemas.UserResponse)
 def read_user(user_id:int,db:Session=Depends(database.get_db)):
     user=db.query(models.User).filter(models.User.id==user_id).first()
     return user
+
+@router.get("/me",response_model=schemas.UserResponse)
+def read_users_me(current_user:models.User=Depends(oauth2.get_current_user)):
+    return current_user
